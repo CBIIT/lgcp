c05b4c6c502dfe65e8e3e1ab4ac94f35cd6a2b7f
bc2zb <brian.capaldo@gmail.com>
2019-02-26 16:17:37 -0500

    begin building of command line edgeR (cledger)

---
+++ rnaseq/cledger.R
@@ -0,0 +1,58 @@
+#!/usr/bin/env Rscript
+
+require(docopt)
+require(methods)
+
+"
+Usage:
+cledger.R (-h | --help | --version)
+cledger.R --normalize RSEM OUT
+
+Description:   This program is a command line interface to edgeR
+
+Options:
+
+--version   Show the current version.
+--normalize Perform filtering and TMM normalization on quantified RNA seq data
+
+Arguments:
+
+RSEM  Provide directory with RSEM .genes.results file(s)
+OUT   Provide output file
+
+" -> doc
+
+args <- docopt(doc)
+
+if(args$`--version` == T){ # returns version if version is requested
+  cat("\nVersion is 0.1\n")
+}else{ # Analysis begins
+  
+  library(edgeR)
+  library(tidyverse)
+  library(tximport)
+  
+  if(args$`--normalize`){
+    dir_name <- args$RSEM
+    file_names <- list.files(path = dir_name,
+                             pattern = ".genes.results$",
+                             full.names = T)
+    txi <- tximport(file_names, type = "rsem", txIn = FALSE, txOut = FALSE)
+    
+    cts <- txi$counts
+    normMat <- txi$length
+    normMat <- normMat/exp(rowMeans(log(normMat)))
+    
+    o <- log(calcNormFactors(cts/normMat)) + log(colSums(cts/normMat))
+    y <- DGEList(cts)
+    y <- scaleOffset(y, t(t(log(normMat)) + o))
+    # filtering
+    keep <- filterByExpr(y)
+    y <- y[keep, ]
+    file_out <- args$OUT
+    cpm(y, log = T) %>%
+      write_csv(file_out)
+  }else{
+    cat(paste(c("\nERROR:","\nCommand not found"), collapse = "\n"), "\n")  
+  }
+}
