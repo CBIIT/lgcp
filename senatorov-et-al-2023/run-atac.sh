8feec7fd352c89f9ef613810305793810d77b82e
bc2zb <capaldobj@nih.gov>
2023-08-29 12:22:51 -0400

    public data run

--- senatorov-et-al-2023/run-atac.sh
+++ senatorov-et-al-2023/run-atac.sh
@@ -13,9 +13,8 @@ module load graphviz
 
 nextflow run nf-core/atacseq --input design.csv \
 -profile biowulf \
--resume \
 --aligner bwa \
 --genome GRCh37 \
 --read_length 75 \
 --narrow_peak \
---outdir '/data/LGCP/freedman-chip/results/'
+--outdir '/data/LGCP/freedman-chip/lucap-only-k27ac-results/'
