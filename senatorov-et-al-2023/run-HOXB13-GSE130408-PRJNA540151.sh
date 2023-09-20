541b9229b99e9e0bf299d4e1efddeddf00278402
bc2zb <capaldobj@nih.gov>
2023-09-20 12:58:19 -0400

    hoxb13 chip of public data init

---
+++ senatorov-et-al-2023/run-HOXB13-GSE130408-PRJNA540151.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+#SBATCH --cpus-per-task=12
+#SBATCH --partition=ccr
+#SBATCH --mem=32gb
+#SBATCH --time=3-00:00:00
+#SBATCH --mail-type=BEGIN,TIME_LIMIT_90,END
+#SBATCH --mail-user=capaldobj@nih.gov
+
+module purge
+module load nextflow
+module load singularity
+module load graphviz
+
+nextflow run nf-core/atacseq -r dev --input /data/capaldobj/lgcp/senatorov-et-al-2023/design-HOXB13-GSE130408-PRJNA540151.csv \
+-profile biowulf \
+--aligner bwa \
+--genome GRCh37 \
+--igenomes_base 's3://ngi-igenomes/igenomes' \
+--read_length 75 \
+--outdir '/data/LGCP/freedman-chip/lucap-only-hoxb13-results/'
