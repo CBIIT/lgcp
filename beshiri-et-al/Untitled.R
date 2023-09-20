819fbf37ae017ef70a55c073dbe598e19d687528
bc2zb <capaldobj@nih.gov>
2023-09-20 10:24:59 -0400

    added reference locations

--- beshiri-et-al/Untitled.R
+++ beshiri-et-al/Untitled.R
@@ -11,3 +11,24 @@
 #### kopycat analysis ####
 
 #### integration analysis ####
+
+# CT35-2
+#        "/data/capaldobj/CS027005_sc_rna_seq_cell_tag/02_PrimaryAnalysisOutput/00_FullCellrangerOutputs/Std_ref/SCAF1427_CT35-2-CTL_1w/outs/filtered_feature_bc_matrix/",
+#        "/data/capaldobj/CS027005_sc_rna_seq_cell_tag/02_PrimaryAnalysisOutput/00_FullCellrangerOutputs/Std_ref/SCAF1512_CT35-2-CTL_4w/outs/filtered_feature_bc_matrix/",
+#        "/data/capaldobj/CS027005_sc_rna_seq_cell_tag/02_PrimaryAnalysisOutput/00_FullCellrangerOutputs/Std_ref/SCAF1513_CT35-2-Ctrl/outs/filtered_feature_bc_matrix/",
+
+        # CT35-1
+#        "/data/capaldobj/CS027005_sc_rna_seq_cell_tag/02_PrimaryAnalysisOutput/00_FullCellrangerOutputs/Std_ref/SCAF1426_CT35-1-CTL_1w/outs/filtered_feature_bc_matrix/",
+#        "/data/capaldobj/CS027005_sc_rna_seq_cell_tag/02_PrimaryAnalysisOutput/00_FullCellrangerOutputs/Std_ref/SCAF1510_CT35-1-CTL_4w/outs/filtered_feature_bc_matrix/",
+#        "/data/capaldobj/CS027005_sc_rna_seq_cell_tag/02_PrimaryAnalysisOutput/00_FullCellrangerOutputs/Std_ref/SCAF1511_CT35-1-Ctrl/outs/filtered_feature_bc_matrix/",
+
+        # MB44
+#        "/data/capaldobj/CS028586_Beshiri_CellTag/02_PrimaryAnalysisOutput/00_FullCellrangerOutputs/Std_ref/SCAF1933_MN44_CTL_112320/outs/filtered_feature_bc_matrix/",
+
+        # 145.2
+#        "/data/capaldobj/CS027004_Kelly_Beshiri_lucap_145.2_cell_tagging/02_PrimaryAnalysisOutput/00_FullCellrangerOutputs/Std_ref/SCAF1385_145_2-CTL-D2/outs/filtered_feature_bc_matrix",
+#        "/data/capaldobj/CS027004_Kelly_Beshiri_lucap_145.2_cell_tagging/02_PrimaryAnalysisOutput/00_FullCellrangerOutputs/Std_ref/SCAF1424_145_2-CTL_3wk/outs/filtered_feature_bc_matrix/"
+
+#       "/data/capaldobj/CS028564_scATAC/02_PrimaryAnalysisOutput/00_FullCellrangerOutputs/SCAF1987_CT35-1/outs/fragments.tsv.gz"
+
+#       /data/capaldobj/CS028564_scATAC/02_PrimaryAnalysisOutput/00_FullCellrangerOutputs/SCAF1988_CT35-2/outs/fragments.tsv.gz
