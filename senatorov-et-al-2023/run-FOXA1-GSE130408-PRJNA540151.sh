2e74107d28f3dff8b7ccf38d94029575fb8c11ae
bc2zb <capaldobj@nih.gov>
2023-10-02 11:46:57 -0400

    added all antibodies to atac pipe runs

--- senatorov-et-al-2023/run-FOXA1-GSE130408-PRJNA540151.sh
+++ senatorov-et-al-2023/run-FOXA1-GSE130408-PRJNA540151.sh
@@ -11,11 +11,11 @@ module load nextflow
 module load singularity
 module load graphviz
 
-nextflow run nf-core/chipseq -r 2.0.0 --input /data/capaldobj/lgcp/senatorov-et-al-2023/design-hnf1a-chip.csv \
+nextflow run nf-core/atacseq -r dev --input /data/capaldobj/lgcp/senatorov-et-al-2023/design-FOXA1-GSE130408-PRJNA540151.csv \
 -profile biowulf \
 --aligner bwa \
 --genome GRCh37 \
 --igenomes_base 's3://ngi-igenomes/igenomes' \
 --narrow_peak \
 --read_length 75 \
---outdir '/data/capaldobj/incoming-nih-dme/CS035088-chip-seq-ilya-hnf1a/chipseq-results/'
+--outdir '/data/LGCP/freedman-chip/lucap-only-foxa1-results/'
